80899f2549a40c23761eee970d8069d13adc8c26
Reanz_Arthur <reqbahrf@gmail.com>
2026-01-13 16:40:46 +0800

    feat: add properties and market-stats sections to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <>
       <section id='hero' className='min-h-[90dvh] bg-gray-50'></section>
+      <section id='properties' className='bg-gray-50'></section>
+      <section id='market-stats' className='bg-gray-50'></section>
     </>
   );
 }
